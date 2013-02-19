e6c151cbcf179788c4846cb93dbf84e7a4870a75
Evan <evan@Evans-MacBook-Pro.local>
2013-02-19 14:59:04 -0500

    added CreatePlatoons function

--- Combs_Evan_Assignment3.js
+++ Combs_Evan_Assignment3.js
@@ -1,15 +1,15 @@
 //arrays
 var generals = [Sampson, Archibald, Salvador, ];
-var platoons = [{},{},{}];
+var platoons = [];
 //numbers
 var selectedGeneral;
 //bools
 var mood;
 
-
+//writes to the console log
 var Write = function(string)
 {
-	consol.log(string);
+	console.log(string);
 }
 
 //this function is used to select who the General will be from the array of generals
@@ -45,4 +45,28 @@ var GeneralsMood = function(string, bool)
 	{
 		write();
 	}
+}
+
+//this creates an array of platoons then returns the array
+var CreatePlatoons = function(number)
+{
+	var leaders = [];
+	var addedPlatoons [];
+	
+	var setSize = function(number)
+	{
+	}
+	
+	var newPlatoon = function(number)
+	{
+		var platoonLeader = leaders[number];
+		var platoonSize = setSize(number);
+	}
+	
+	for (var i = 0; i < number; i++)
+	{
+		addedPlatoons.push(newPlatoon(i));
+	}
+	
+	return addedPlatoons;
 }
