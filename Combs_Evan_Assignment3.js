69adf02ded029c90964d9c7b0751e5c3eb8666ec
Evan <evan@Evans-MacBook-Pro.local>
2013-02-19 14:51:40 -0500

    first functions

--- Combs_Evan_Assignment3.js
+++ Combs_Evan_Assignment3.js
@@ -1,1 +1,48 @@
-alert("JavaScript works!");
+//arrays
+var generals = [Sampson, Archibald, Salvador, ];
+var platoons = [{},{},{}];
+//numbers
+var selectedGeneral;
+//bools
+var mood;
+
+
+var Write = function(string)
+{
+	consol.log(string);
+}
+
+//this function is used to select who the General will be from the array of generals
+var SelectGeneral = function()
+{
+	var random = Math.Floor(Math.random()) * 4;
+	return random;
+}
+
+//this function uses who the general is and his mood to decide what the general should say
+var GeneralsMood = function(string, bool)
+{
+	if (bool === true)
+	{
+		if (string === generals[0])
+		{
+			write();
+		}
+		else if (string === generals[1])
+		{
+			write();
+		}
+		else if (string === generals[2])
+		{
+			write();
+		}
+		else
+		{
+			write();
+		}
+	}
+	else
+	{
+		write();
+	}
+}
